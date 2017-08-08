4106fe18db4654f98dabd623588d264a21f51171
Senthil Padmanabhan <spadmanabhan@ebay.com>
2017-08-07 17:29:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ module.exports = {
         'eslint-config-ebay/legacy',
         'eslint-config-ebay/rules/es6'
     ].map(require.resolve),
+    parserOptions: {
+        ecmaVersion: 2017
+    },    
     "rules": {}
 };
